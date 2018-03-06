93b1753d39c9c345cb3076a5f48694c1278588d7
Monali Patil <monali24u@gmail.com>
2018-03-06 13:36:35 -0700

    declaring variables using let,const and var

---
+++ letAndConst.js
@@ -0,0 +1,13 @@
+function main() {
+
+    const PI = Math.PI;
+    // Write your code here. Read input using 'readLine()' and print output using 'console.log()'.
+    let r = readLine();
+
+    // Print the area of the circle:
+    var area = PI * r * r;
+    console.log(area);
+    // Print the perimeter of the circle:
+    var perimeter = 2 * PI * r;
+    console.log(perimeter);
+}
