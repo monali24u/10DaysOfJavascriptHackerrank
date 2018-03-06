ef3dbbe87dee31f4b53009dcbf4f889b8c427507
Monali Patil <monali24u@gmail.com>
2018-03-06 11:47:12 -0700

    greeting function to print content

---
+++ helloworld.js
@@ -0,0 +1,11 @@
+
+function greeting(parameterVariable) {
+    // This line prints 'Hello, World!' to the console:
+    console.log('Hello, World!');
+
+    // Write a line of code that prints parameterVariable to stdout using console.log:
+    console.log(parameterVariable);
+}
+
+var parameterVariable = "Welcome to 10 Days of JavaScript!"
+greeting(parameterVariable)
