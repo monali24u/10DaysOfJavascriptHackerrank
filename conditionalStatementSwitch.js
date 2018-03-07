42c15e6ed90d9180bad504327202d6cb1d1f4d66
Monali Patil <monali24u@gmail.com>
2018-03-07 08:25:06 -0700

    Switch Conditional statement to get letter

---
+++ conditionalStatementSwitch.js
@@ -0,0 +1,38 @@
+function getLetter(s) {
+    let letter;
+    // Write your code here
+
+    let firstchar = s[0]
+
+    switch(firstchar)
+    {
+        case 'a':
+        case 'e':
+        case 'i':
+        case 'o':
+        case 'u':
+            letter = "A";
+            break;
+        case 'b':
+        case 'c':
+        case 'd':
+        case 'f':
+        case 'g':
+            letter = "B";
+            break;
+        case 'h':
+        case 'j':
+        case 'k':
+        case 'l':
+        case 'm':
+            letter = "C";
+            break;
+        default:
+            letter = "D";
+    }
+
+
+    return letter;
+}
+
+getLetter("monalipatil");
